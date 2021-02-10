e8640d9b12cc11ba9914940636b8e754292da453
Freakando <74593274+Freakando@users.noreply.github.com>
2021-02-10 11:06:33 +0100

    project envirolment set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+//import reset.scss
+import './App.scss';
+import {Template} from "./components/template/Template";
 
 function App() {
   return (
     <div className="App">
+      <Template />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/template/Template.js
@@ -0,0 +1,7 @@
+import("./Template.scss");
+
+
+export const Template = () =>
+    <header className={"template--header"}>
+        <h1></h1>
+    </header>
